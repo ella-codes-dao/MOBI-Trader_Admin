b906d90113d07389911677911ec9c1e1c90e4efd
BoiseITGuru <dev@boiseitguru.com>
2022-08-04 12:30:39 -0600

    Initial Commit

---
+++ MOBI Trader Admin/MOBI_Trader_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBI_Trader_AdminApp.swift
+//  MOBI Trader Admin
+//
+//  Created by BoiseITGuru on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MOBI_Trader_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
